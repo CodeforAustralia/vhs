c199a19ce318ed5f16f86041c07d8c3a7b42f4dc
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-09-05 12:04:45 +1000

    Changed Footer copyright
    Signed-off-by: Rachelle Salvadora <rachellesalvadora@gmail.com>

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -3,7 +3,7 @@
 <!-- FOOTER -->
 <footer>
   <p class="pull-right"><a href="#">Back to top</a></p>
-  <p>&copy; Ministry of Magic. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  <p>&copy; VHS 2017 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 </footer>
 </div>
 
