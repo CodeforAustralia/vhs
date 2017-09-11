ad0757bb49e82825505f60e5377bf96b5a16e26b
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-09-11 20:52:53 +1000

    Quick Escape button

---
+++ public/js/custom.js
@@ -0,0 +1,2 @@
+/* Reminiscent of the 'Hide Page' button on http://www.vic.gov.au/familyviolence/recommendations.html */
+$('.btn-danger').on('click',function(e){window.location.href='https://www.google.com.au/';});
--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -63,4 +63,4 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
-
+<script src="{{ asset('js/custom.js') }}"></script>
