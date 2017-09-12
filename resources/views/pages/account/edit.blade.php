26c2fa218ef4b41897f1101b05d19acae1316ce2
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-09-12 16:46:10 +1000

    Edited Accounts Style, Links on sections title for easier navigation

--- resources/views/pages/account/edit.blade.php
+++ resources/views/pages/account/edit.blade.php
@@ -8,7 +8,27 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-primary">
-				<div class="panel-heading">Account Details</div>
+				<div class="panel-heading">
+				<div class="col-md-2">
+
+						</div>
+						<div class="col-md-7 services_heading_right">
+							@if ( Auth::user()->usertype == '1' )
+							<h3><a href="/accounts">All Accounts</a></h3>
+							@else
+							<h3>Account Details</h3>
+							@endif
+						</div>
+						<div class="col-md-2 services_heading_right">
+							<div class="toolbox-banner mail_icon-link">
+								<p class="pull-right"><a class="panel-heading_link" href="/accountdetails/{{ $AccountDetails[0]->id }}">Back to Profile</a></p>
+							</div>
+						</div>
+						<div class="col-md-1 services_heading_right">
+							<div class="toolbox-banner mail_icon-link">
+							</div>
+						</div>
+				</div>
 				<div class="panel-body">
 					@if(Session::has('message'))
 					<p class="alert alert-danger">{{ Session::get('message') }}</p>
