ac85fa0e0ca6766acbfdcd7a14bfbae7e27feb98
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-09-12 21:26:59 +1000

    Add JS

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,2 +1,23 @@
-/* Reminiscent of the 'Hide Page' button on http://www.vic.gov.au/familyviolence/recommendations.html */
+/* Quick Escape
+- reminiscent of the 'Hide Page' button on http://www.vic.gov.au/familyviolence/recommendations.html */
 $('.btn-danger').on('click',function(e){window.location.href='https://www.google.com.au/';});
+
+/* Toggle service panels to show all letters */
+$('.show-more').on('click',function(e){
+  current_text = $(this).text();
+  if (current_text == "Show More") {
+    $(this).parent().find(".row").show();
+    $(this).parent().find(".row").first().css("padding-bottom","0");
+    current_text = "Show Less";
+  } else {
+    $(this).parent().find(".row").hide();
+    $(this).parent().find(".row").first().show();
+    $(this).parent().find(".row").first().css("padding-bottom","10px");
+
+    current_text = "Show More";
+  }
+  $(this).text(current_text);
+});
+
+// Trigger first click to collapse all service panels
+$( ".show-more" ).trigger( "click" );
