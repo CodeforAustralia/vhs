a7e32eaf1b50c012e19baaa47f4f8b5b77064715
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-09-12 16:31:39 +1000

    Changed pagination to show data  in alphabetical order

--- app/Http/Controllers/AccountDetailsController.php
+++ app/Http/Controllers/AccountDetailsController.php
@@ -30,7 +30,7 @@ class AccountDetailsController extends Controller
     public function index()
     {
         // Get all account details
-        $AccountDetails = AccountDetails::paginate(15);
+        $AccountDetails = AccountDetails::orderBy('firstName')->paginate(15);
         // Get all account address details
         // $UserAddress = UserAddress::all();
 
