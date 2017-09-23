325ec48a4e73ce03d1bd20a4c5da16ca1d60ee01
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-09-23 14:50:18 +1000

    Put in  as variable

--- resources/views/pages/letter.blade.php
+++ resources/views/pages/letter.blade.php
@@ -9,11 +9,16 @@
       <div class="row">
 
 <!-- Panel -->
+<?php
+$protocol=getenv('SERVER_PROTOCOL');
+$server=getenv('SERVER_ADDR') ;
+?>
 
 @foreach ($letters as $letter)
                    <!-- Letter -->
                      <div style="width:100%;height:100%;">
-                     <iframe src="http://localhost:8080/actual-letter" height="800px" width="100%"></iframe>
+                     <iframe src="{{$protocol}}{{$server}}/actual-letter" height="800px" width="100%">
+                     </iframe>
                      </div>
 @endforeach
 
