6a5a8f07f25e65867751bc6b1005b996fff2d541
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-10-01 21:41:55 +1100

    Issue #16, Issue #5 - Added Aloha/twilio library to json file and Added user_letters_table
    Signed-off-by: Rachelle Salvadora <rachellesalvadora@gmail.com>

---
+++ database/migrations/2017_09_25_014031_create_user_letters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserLettersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Create user_letters table
+        Schema::create('user_letters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('letter_id')->unsigned();
+            $table->foreign('letter_id')->references('id')->on('letters')->onDelete('cascade');
+            $table->bigInteger('service_id')->unsigned();
+            $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('unread')->default(true);
+            $table->timestamp('opened_at')->nullable();
+            $table->timestamp('received_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
