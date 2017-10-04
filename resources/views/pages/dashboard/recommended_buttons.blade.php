77c94b7e93e9ed817adbbf8b9ca6a533f82b6c85
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-10-05 10:26:52 +1100

    recommended buttons change - testing
    Signed-off-by: Rachelle Salvadora <rachellesalvadora@gmail.com>

--- resources/views/pages/dashboard/recommended_buttons.blade.php
+++ resources/views/pages/dashboard/recommended_buttons.blade.php
@@ -4,7 +4,7 @@
             <!-- <div class="col-md-8 col-md-offset-2"> -->
             <div class="col-md-8 recommended_links-wrapper">
                 <div class="recommended_buttons col-md-12 col-lg-12">
-                <a href="#" target="_blank" role="button" class="btn btn-default need_housing_button">{!! file_get_contents(asset('images/ico-urgenthousing.svg')) !!} <span>Need urgent housing?</span></a>
+                <a href="#" target="_blank" role="button" class="btn btn-default need_housing_button">{!! file_get_contents(asset(public_path('images/ico-urgenthousing.svg'))) !!} <span>Need urgent housing?</span></a>
                 </div>
                 <div class="recommended_buttons col-md-12 col-lg-12">
                     <a href="http://housing.vic.gov.au/housing-options-finder" target="_blank" role="button" class="btn btn-default housing_options_button">{!! file_get_contents(asset('images/ico-findyourhousing.svg')) !!} <span>Find your Housing Options</span></a>
