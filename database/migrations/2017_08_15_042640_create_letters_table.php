662761daa21db1748c0ee73a87dc6ce77d11a077
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-10-11 06:46:07 +1100

    Add pages to letters table

--- database/migrations/2017_08_15_042640_create_letters_table.php
+++ database/migrations/2017_08_15_042640_create_letters_table.php
@@ -20,6 +20,7 @@ class CreateLettersTable extends Migration
             $table->string('template_id');
             $table->string('filename');
             $table->date('letter_date');
+            $table->integer('pages');
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
         });
