bc27a1c2499787c06a68d9fb29ebed5dd616be1b
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-10-12 05:33:56 +1100

    Added documenation for testing

---
+++ public/testing.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,‌​100italic,300,300ita‌​lic,400italic,500,50‌​0italic,700,700itali‌​c,900italic,900' rel="stylesheet">
+  <style>
+    body {
+          font-family: "HelveticaNeue", Roboto, Arial, sans-serif;
+          font-size: 16px;
+    }
+    .grey {
+      color: grey;
+    }
+  </style>
+</head>
+<body>
+  <h1>Wi-fi Access</h1>
+  SSID: TP4G_032B0A<br/>
+  Password: JDR48Q2GY3
+
+  <h1>Non-functional Prototype</h1>
+  <a href="https://invis.io/MCDW9MXT4" target="_blank">Option A</a><br/>
+  <a href="https://invis.io/RVDW9N6UE" target="_blank">Option B</a>
+
+  <h1>System being Developed</h1>
+  <a href="https://vhstesting.com" target="_blank">https://vhstesting.com</a><br/>
+  Login in with the email given to you on a slip of paper<br>
+  This will be one of:-
+  <ul style="list-style-type: square;">
+    <li><span class="grey">samanderson@gmail.com</span></li>
+    <li><span class="grey">alexsmith@gmail.com</span></li>
+    <li><span class="grey">daleskiles@gmail.com</span></li>
+  </ul>
+  Password: <span class="grey">8@C3U!</span>
+</body>
+</html>
