83ad1ab3f433697d24cb3e4770761081e7baaeaa
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-10-11 21:01:22 +1100

    Fixed indenting of LettersListController
    Signed-off-by: Rachelle Salvadora <rachellesalvadora@gmail.com>

--- app/Http/Controllers/LettersListController.php
+++ app/Http/Controllers/LettersListController.php
@@ -13,10 +13,10 @@ use App\Models\LetterHistory;
 class LettersListController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    * Create a new controller instance.
+    *
+    * @return void
+    */
     public function __construct()
     {
         $this->middleware('auth');
@@ -25,45 +25,46 @@ class LettersListController extends Controller
     public function index($reference_id)
     {
     // check to see if this service belongs to the logged-in user
-     $user_id = Auth::user()->id;
-     $user_services = UserService::where('user_id',$user_id)
-      ->where('reference_id',$reference_id)
-      ->get();
+        $user_id = Auth::user()->id;
+        $user_services = UserService::where('user_id',$user_id)
+        ->where('reference_id',$reference_id)
+        ->get();
 
 
-    if (count($user_services) == 0) { // if service does not belong, re-route to the dashboard
-      return redirect()->route('dashboard');
+        if (count($user_services) == 0) { // if service does not belong, re-route to the dashboard
+            return redirect()->route('dashboard');
+        }
+
+        return view('letter/lettersList')->with([
+            'user_services' => $user_services,
+        'include_links' => false   // do not include links on summary of services and new letters
+        ]);
     }
 
-    return view('letter/lettersList')->with([
-         'user_services' => $user_services,
-         'include_links' => false   // do not include links on summary of services and new letters
-       ]);
-   }
+    public function show($id) {
 
-   public function show($id) {
+        $letters = Letters::where('id', $id)->get();
+        /* start */
+        if (count($letters) > 0) {
+        // first check to see if this letter belongs to the logged-in user
+            $user_id = Auth::user()->id;
+            $letter_history = LetterHistory::where('user_id',$user_id)
+            ->where('letter_uuid', $letters[0]->uuid)
+            ->get();
+        }
 
-     $letters = Letters::where('id', $id)->get();
-/* start */
-    if (count($letters) > 0) {
-         // first check to see if this letter belongs to the logged-in user
-         $user_id = Auth::user()->id;
-         $letter_history = LetterHistory::where('user_id',$user_id)
-         ->where('letter_uuid', $letters[0]->uuid)
-         ->get();
+        if ( empty($letters) || empty($letter_history) ) { // if letter is not meant for this user, re-route to the dashboard
+            return redirect()->route('dashboard');
+        }
+
+        // if we have made it this far, mark the letter as read
+        $letter_history[0]->unread=false;
+        $letter_history[0]->save();
+
+        /* end */
+        return view('letter/letter')->with([
+            'letters' => $letters
+            ]);
+        }
     }
-
-    if ( empty($letters) || empty($letter_history) ) { // if letter is not meant for this user, re-route to the dashboard
-         return redirect()->route('dashboard');
-    }
-
-// if we have made it this far, mark the letter as read
-    $letter_history[0]->unread=false;
-    $letter_history[0]->save();
-
-/* end */
-     return view('letter/letter')->with([
-       'letters' => $letters
-     ]);
-  }
- }
+}
