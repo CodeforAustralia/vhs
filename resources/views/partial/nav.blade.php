4b3c8791c6896fe6bec9b488bb434f5cd8e4198d
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-10-15 23:55:37 +1100

    Finalising navbar

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -22,30 +22,74 @@
     @else
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="/">Start Page</a></li>
+        <!-- Start Page -->
+        <li>
+          <a href="/">
+            <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-start-page.svg')) !!}</div>&nbsp;Start Page
+          </a>
+        </li>
+        <!-- Dropdown Menu -->
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Go to <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li>
               @if ( Auth::user()->usertype == '1' )
-              <a href="/accounts">User Accounts</a>
+              <a href="/accounts">
+                <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-start-page.svg')) !!}</div>&nbsp;User Accounts
+              </a>
               @else
-              <a href="/accounts/{{ Auth::user()->id }}">My Profile</a>
+              <a href="/accounts/{{ Auth::user()->id }}">
+                <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-profile.svg')) !!}</div>&nbsp;My Profile
+              </a>
               @endif
             </li>
-            <li><a href="/inbox">Inbox</a></li>
-            <li><a href="/#">Services</a></li>
-            <li><a href="/#">Make Payment</a></li>
-            <li><a href="/#">Appointments</a></li>
-            <li><a href="/#">Translation</a></li>
+
+            <!-- Inbox -->
+            <li>
+              <a href="/inbox">
+               <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-inbox.svg')) !!}</div>&nbsp;Inbox
+              </a>
+            </li>
+            <!-- Services -->
+            <li>
+              <a href="#">
+               <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-services.svg')) !!}</div>&nbsp;Services
+              </a>
+            </li>
+            <!-- Make Payment -->
+            <li>
+              <a href="#">
+               <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-payment.svg')) !!}</div>&nbsp;Make Payment
+              </a>
+            </li>
+            <!-- Appointments -->
+            <li>
+              <a href="#">
+               <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-appointments.svg')) !!}</div>&nbsp;Appointments
+              </a>
+            </li>
+            <!-- Translation -->
+            <li>
+              <a href="#">
+               <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-translation.svg')) !!}</div>&nbsp;Translation
+              </a>
+            </li>
           </ul>
         </li>
-        <li class="bottom_nav_items"><a href="/#">Help</a></li>
+        <!-- Help -->
+        <li class="bottom_nav_items">
+          <a href="#">
+            <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-help.svg')) !!}</div>&nbsp;Help
+          </a>
+        </li>
+        <!-- Log out -->
+
         <li class="bottom_nav_items">
             <a href="{{ route('logout') }}"
             onclick="event.preventDefault();
             document.getElementById('logout-form').submit();">
-            Logout</a>
+              <div class="navbar-menu-image">{!! file_get_contents(public_path('images/ico-logout.svg')) !!}</div>&nbsp;Log out
+        </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 {{ csrf_field() }}
             </form>
