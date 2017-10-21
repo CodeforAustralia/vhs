febb848471311af5e720e611e62324898122becf
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-10-21 18:15:57 +1100

    Bigger Quick Exit button

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -20,7 +20,7 @@ header("Expires: 0"); // Proxies
 
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Cache Control -->
   <meta http-equiv="Cache-Control" content="Cache-Control: no-cache, max-age=0, must-revalidate, no-store" />
