0b994debc9f000f40cbf73a087426afba2b12c44
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-10-22 20:38:36 +1100

    Align footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,63 +1,59 @@
 <div class="container-wrapper quick_links-wrapper">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-9 footer-wrapper">
-        <div class="col-md-4 quicklinks_half">
-          <div class="quicklinks-title">
-            <h4>Help with information</h4>
-          </div>
-          <div>
-            <ul>
-              <li>Housing.vic.gov.au</li>
-              <li>National Relay Service</li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-4 quicklinks_half">
-          <div class="quicklinks-title">
-            <h4>About this application</h4>
-          </div>
-          <div>
-            <ul>
-              <li>Accessibility statement</li>
-              <li>Privacy</li>
-              <li>Disclaimer</li>
-              <li>Copyright</li>
-            </ul>
-          </div>
-        </div>
-<!--
-        <div class="col-md-4 quicklinks_half">
-          <div class="quicklinks-title">
-            <h4>Recommended Links</h4>
-          </div>
-          <div>
-            <p>Looking into private rental? <a href="#">Learn more about VHS Bonding Assistance.</a></p>
-            <p>Need advice and support? <a href="#">Find a service provider near you</a></p>
-            <p><a href="#">Nearest Housing Office</a></p>
-          </div>
-        </div>
--->
-      </div>
-    </div> <!-- /.row -->
-    <div class="row">
-        <!-- FOOTER -->
-				  <footer>
-          <div class="col-md-8">
-            <p class="">Pre-ALPHA Release</p>
-            <p>&copy; State Government of Victoria
-              <?php echo date("Y"); ?>
-            </p>
-            <p class="">To be authorised by: Department of Health and Human Services</p>
-          </div>
-          <div class="col-md-4">
-            <p class="pull-right">
-              <a href="#"><img src="{{ asset('images/logo-vicstategov.png') }}" /></a>
-            </p>
-          </div>
-				</footer>
-    </div> <!-- /.row -->
-</div> <!-- /.container -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 footer-wrapper">
+				<div class="col-md-4 quicklinks_half">
+					<div class="quicklinks-title">
+						<h4>Help with information</h4>
+					</div>
+					<div>
+						<ul>
+							<li>Housing.vic.gov.au</li>
+							<li>National Relay Service</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 quicklinks_half">
+					<div class="quicklinks-title">
+						<h4>About this application</h4>
+					</div>
+					<div>
+						<ul>
+							<li>Accessibility statement</li>
+							<li>Privacy</li>
+							<li>Disclaimer</li>
+							<li>Copyright</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 quicklinks_half">
+					<!-- <div class="quicklinks-title">
+						<h4>Recommended Links</h4>
+					</div>
+					<div>
+						<p>Looking into private rental? <a href="#">Learn more about VHS Bonding Assistance.</a></p>
+						<p>Need advice and support? <a href="#">Find a service provider near you</a></p>
+						<p><a href="#">Nearest Housing Office</a></p>
+					</div> -->
+				</div>
+			</div>
+		</div><!-- /.row -->
+			<div class="row">
+				<div class="col-md-8 footer-wrapper">
+					<!-- FOOTER -->
+					<footer>
+						<div class="col-md-8">
+							<p>Pre-ALPHA Release</p>
+							<p>&copy; State Government of Victoria <?php echo date("Y"); ?></p>
+							<p>To be authorised by: Department of Health and Human Services</p>
+						</div>
+						<div class="col-md-4">
+							<p class="pull-right"><a href="#"><img src="{{ asset('images/logo-vicstategov.png') }}"/></a></p>
+						</div>
+					</footer>
+				</div>
+			</div> <!-- /.row -->
+	</div> <!-- /.container -->
 </div> <!-- /.container-wrapper -->
 
 <!-- Scripts -->
