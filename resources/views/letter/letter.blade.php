d2ec55bcad73218bc2ce4f5959fc55c9a18f1843
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-10-30 10:34:58 +1100

    Resize Print and Download buttons

--- resources/views/letter/letter.blade.php
+++ resources/views/letter/letter.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app') @section('title', 'Home') @section('content')
+@foreach ($letters as $letter)
 
-<!-- Buttons -->
+
 <div class="container-wrapper">
   <div class="container">
+    <!-- Template summary -->
     <div class="row">
-      <div class="col-md-8">
+      <div class="col-md-8 welcome_banner-wrapper">
+        <div class="welcome-banner">
+          <h3>{{$letter->template->summary}}</h3>
+        </div>
+      </div>
+    </div>
+
+    <!-- Buttons -->
+    <div class="row">
+      <div class="col-md-8 welcome_banner-wrapper">
         <a id="rippleDownload" class="btn btn-vhs" href="#navigation-main" aria-label="Download">
           {!! file_get_contents(public_path('images/download.svg')) !!}</i>&nbsp;&nbsp;Download
         </a>
@@ -16,35 +27,26 @@
         </a>
       </div>
     </div>
+
   </div>
 </div>
 
-<!-- Loader
-<div class="container-wrapper tools-wrapper loading-wrapper">
-  <div class="container">
-    <div class="row">
-
-    </div>
-  </div>
-</div>
--->
-
-<!-- Letter -->
 <div class="container-wrapper tools-wrapper no-padding with-top-margin">
   <div class="container">
     <div class="row">
-
+      <!-- Loader -->
       <div class="loading">
         <img src="{{ asset('images/loading.gif') }}"/>
       </div>
-
-      @foreach ($letters as $letter)
-      <div class="embed-responsive pdf2html-wrapper">
-        <iframe id="letterIframe" class="embed-responsive-item" src="/letter-pdfjs/{{$letter->id}}#zoom=page-width" allowfullscreen></iframe>
+      <!-- Letter -->
+<!--      <div class="col-md-8 welcome_banner-wrapper"> -->
+        <div class="embed-responsive pdf2html-wrapper">
+          <iframe id="letterIframe" class="embed-responsive-item" src="/letter-pdfjs/{{$letter->id}}#zoom=page-width" allowfullscreen></iframe>
+        </div>
       </div>
-      @endforeach
-    </div>
+<!--    </div> -->
   </div>
 </div>
 
+@endforeach
 @endsection
