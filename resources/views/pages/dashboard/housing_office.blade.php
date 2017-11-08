4aaae8b7390bd8eac0549e68b9bf9c2f97f472c9
TattyFromMelbourne <tlenz@lensoftware.com.au>
2017-11-08 16:37:17 +1100

    MVP Front Page cahnges

--- resources/views/pages/dashboard/housing_office.blade.php
+++ resources/views/pages/dashboard/housing_office.blade.php
@@ -2,32 +2,34 @@
 <div class="container-wrapper services-wrapper">
   <div class="container">
     <div class="row odd-row">
-      <div class="col-md-8  col-sm-8  col-xs-12 recommendation_buttons-wrapper">
-        <div class="container indent-content">
-          <div class="col-md-12">
-            <div class="ho-header">My Housing Office</div>
+      <div class="col-md-8 col-sm-8 col-xs-12 recommendation_buttons-wrapper">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="ho-header">My Housing Office</div>
+            </div>
           </div>
-        </div>
-        <div class="col-md-5 col-sm-7 ho-text-wrapper">
-          <div class="ho-image-marker">
-            <i class="fa fa-map-marker fa-2x"></i>
-          </div>
-          <div class="ho-text">
-            Fitzroy Office
-            <br/>145 Smith St, Fitzroy VIC 3065
-          </div>
-        </div>
-        <div class="col-md-5 col-sm-5 ho-text-wrapper">
-          <div class="ho-image-marker">
-            <i class="fa fa-phone fa-2x"></i>
-          </div>
-          <div class="ho-text">
-            <a href="#">
-                1300&nbsp;360&nbsp;408
-              </a>
+          <div class="row">
+            <div class="col-md-5 col-sm-5 ho-text-wrapper">
+              <div class="ho-image-marker">
+                <i class="fa fa-map-marker fa-2x"></i>
+              </div>
+              <div class="ho-text">
+                Fitzroy Office<br/>
+                145 Smith St, Fitzroy VIC 3065
+              </div>
+            </div>
+            <div class="col-md-7 col-sm-7 ho-text-wrapper">
+              <div class="ho-image-marker">
+                <i class="fa fa-phone fa-2x"></i>
+              </div>
+              <div class="ho-text">
+                <a href="#">1300&nbsp;360&nbsp;408</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
+    </div> <!-- /.row -->
+  </div> <!-- /.container -->
+</div> <!-- /.container-wrapper -->
--- resources/views/pages/dashboard/recommended_buttons.blade.php
+++ resources/views/pages/dashboard/recommended_buttons.blade.php
@@ -1,30 +1,34 @@
 <!-- External Sites Buttons -->
 <div class="container-wrapper recommended_buttons_tools-wrapper">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-sm-8 col-xs-12 recommended_links-wrapper">
-                <div class="recommended_buttons col-md-12 col-lg-12">
-                  <a href="#" target="_blank" role="button" class="need_housing_button">
-                    {!! file_get_contents(public_path('images/ico-urgenthousing.svg')) !!}
-                    <span>Need urgent housing?</span>
-                    <span class="external-link">{!! file_get_contents(public_path('images/ico-external-link.svg')) !!}</span>
-                  </a>
-                </div>
-                <div class="recommended_buttons col-md-12 col-lg-12">
-                  <a href="http://housing.vic.gov.au/housing-options-finder" target="_blank" role="button" class="housing_options_button">
-                    {!! file_get_contents(public_path('images/ico-findyourhousing.svg')) !!}
-                    <span>Find your Housing Options</span>
-                    <span class="external-link">{!! file_get_contents(public_path('images/ico-external-link.svg')) !!}</span>
-                  </a>
-                </div>
-                <div class="recommended_buttons col-md-12 col-lg-12">
-                  <a href="http://housing.vic.gov.au/housing-options-finder" target="_blank" role="button" class="housing_options_button">
-                    {!! file_get_contents(public_path('images/ico-nearest-ho.svg')) !!}
-                    <span>Nearest Housing Office</span>
-                    <span class="external-link">{!! file_get_contents(public_path('images/ico-external-link.svg')) !!}</span>
-                  </a>
-                </div>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-sm-8 col-xs-12 recommended_links-wrapper">
+        <div class="recommended_buttons">
+          <a href="#" target="_blank" role="button" class="need_housing_button">
+            {!! file_get_contents(public_path('images/ico-urgenthousing.svg')) !!}
+            <span>Need Urgent Housing?</span>
+            <span class="external-link">{!! file_get_contents(public_path('images/ico-external-link.svg')) !!}</span>
+          </a>
         </div>
-    </div>
-</div>
+      </div>
+      <div class="col-md-8 col-sm-8 col-xs-12 recommended_links-wrapper">
+        <div class="recommended_buttons">
+          <a href="#" target="_blank" role="button" class="need_housing_button">
+            {!! file_get_contents(public_path('images/ico-nearest-ho.svg')) !!}
+            <span>Find nearest Housing Office</span>
+            <span class="external-link">{!! file_get_contents(public_path('images/ico-external-link.svg')) !!}</span>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-8 col-sm-8 col-xs-12 recommended_links-wrapper">
+        <div class="recommended_buttons">
+          <a href="http://housing.vic.gov.au/housing-options-finder" target="_blank" role="button" class="need_housing_button">
+            {!! file_get_contents(public_path('images/ico-findyourhousing.svg')) !!}
+            <span>Find your Housing Options</span>
+            <span class="external-link">{!! file_get_contents(public_path('images/ico-external-link.svg')) !!}</span>
+          </a>
+        </div>
+      </div>
+    </div><!-- /.row -->
+  </div><!-- /.container -->
+</div><!-- /.container-wrapper -->
