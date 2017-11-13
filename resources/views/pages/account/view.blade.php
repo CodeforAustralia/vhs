80c0798b6f02d95895ed2b28c7e661200ae7bbeb
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-11-13 12:04:35 +1100

    Added vhs.css, Changed View Account Details Header, Added vhs.css to header
    Signed-off-by: Rachelle Salvadora <rachellesalvadora@gmail.com>

--- resources/views/pages/account/view.blade.php
+++ resources/views/pages/account/view.blade.php
@@ -14,73 +14,75 @@
 				<h4 class="modal-title">Send Sample Letters</h4>
 			</div>
 			<div class="modal-body">
-				<p>Use this is for testing purposes only. You are essentailly assigning
+				<p>
+					Use this is for testing purposes only. You are essentailly assigning
 					new services to Social Housing clients and simulating the Department
 					sending letters in regard to those services.
-					<b>DO NOT USE IN A PRODUCTION SYSTEM!</b></p>
-					<p>&nbsp;</p>
-					<form class="form-horizontal" method="POST" action="{{ route('correspondence.sendLetters', $AccountDetails[0]->id) }}">
-						{{ csrf_field() }}
-						@include('letter.simulateLetters')
-					</div>
-					<div class="modal-footer">
-						<button type="submit" class="btn btn-primary">Send</button>
-						<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-					</div>
-				</form>
-			</div>
+					<b>DO NOT USE IN A PRODUCTION SYSTEM!</b>
+				</p>
+				<p>&nbsp;</p>
+				<form class="form-horizontal" method="POST" action="{{ route('correspondence.sendLetters', $AccountDetails[0]->id) }}">
+					{{ csrf_field() }}
+					@include('letter.simulateLetters')
+				</div>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-primary">Send</button>
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				</div>
+			</form>
 		</div>
 	</div>
-	@endif
+</div>
+@endif
 
-	<!-- Get session messages -->
-	@if (Session::has('message.content'))
-	<div id="sessionStatus" class="alert alert-{{ Session::get('message.level') }}">
-		{{Session::get('message.content')}}
-		<div class="closeCrossDiv"><a href="javascript: hideObject('#sessionStatus');">&times;</a></div>
-	</div>
-	@endif
+<!-- Get session messages -->
+@if (Session::has('message.content'))
+<div id="sessionStatus" class="alert alert-{{ Session::get('message.level') }}">
+	{{Session::get('message.content')}}
+	<div class="closeCrossDiv"><a href="javascript: hideObject('#sessionStatus');">&times;</a></div>
+</div>
+@endif
 
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-primary">
-					<div class="panel-heading">
-						<div class="welcome-banner">
-							<div class="col-md-2">
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<div class="welcome-banner">
+						<div class="col-md-2">
 
-							</div>
-							<div class="col-md-7 services_heading_right">
-								@if ( Auth::user()->usertype == '1' )
-								<h3>{{ $AccountDetails[0]->firstName }} {{ $AccountDetails[0]->lastName }}</h3>
-								<p>
-									@if($AccountDetails[0]->last_login_at == NULL)
-									{{ 'This user has never logged in before' }}
-									@else
-									{{$lastLoginAt = Request::get($AccountDetails[0]->last_login_at) }}{{ 'Last login '. date("F jS, Y h:i:s A", strtotime($lastLoginAt)) }}
-									@endif
-								</p>
+						</div>
+						<div class="col-md-7 services_heading_right">
+							@if ( Auth::user()->usertype == '1' )
+							<h3>{{ $AccountDetails[0]->firstName }} {{ $AccountDetails[0]->lastName }}</h3>
+							<p>
+								@if($AccountDetails[0]->last_login_at == NULL)
+								{{ 'This user has never logged in before' }}
 								@else
-								<h3>{{ $AccountDetails[0]->firstName }} {{ $AccountDetails[0]->lastName }}</h3>
-								<p>
-									@if($AccountDetails[0]->last_login_at == NULL)
-									{{ 'This user has never logged in before' }}
-									@else
-									{{$lastLoginAt = Request::get($AccountDetails[0]->last_login_at) }}{{ 'Last login '. date("F jS, Y h:i:s A", strtotime($lastLoginAt)) }}
-									@endif
-								</p>
+								{{$lastLoginAt = Request::get($AccountDetails[0]->last_login_at) }}{{ 'Last login '. date("F jS, Y h:i:s A", strtotime($lastLoginAt)) }}
 								@endif
+							</p>
+							@else
+							<h3>{{ $AccountDetails[0]->firstName }} {{ $AccountDetails[0]->lastName }}</h3>
+							<p>
+								@if($AccountDetails[0]->last_login_at == NULL)
+								{{ 'This user has never logged in before' }}
+								@else
+								{{$lastLoginAt = Request::get($AccountDetails[0]->last_login_at) }}{{ 'Last login '. date("F jS, Y h:i:s A", strtotime($lastLoginAt)) }}
+								@endif
+							</p>
+							@endif
 
+						</div>
+						<div class="col-md-3 services_heading_right">
+							<div class="toolbox-banner mail_icon-link">
+								@if ( Auth::user()->usertype == '1' )
+								<p><a class="panel-heading_link" href="/notification/{{ $AccountDetails[0]->id }}">Send Notification</a></p>
+								<p><a class="panel-heading_link" href="#modalWindow" data-toggle="modal" data-target="#modalWindow">Send Sample Letters</a></p>
+								<p><a href="/accounts">All Users</a></p>
+								@endif
 							</div>
-							<div class="col-md-3 services_heading_right">
-								<div class="toolbox-banner mail_icon-link">
-									@if ( Auth::user()->usertype == '1' )
-									<p><a class="panel-heading_link" href="/notification/{{ $AccountDetails[0]->id }}">Send Notification</a></p>
-									<p><a class="panel-heading_link" href="#modalWindow" data-toggle="modal" data-target="#modalWindow">Send Sample Letters</a></p>
-									<p><a href="/accounts">All Accounts</a></p>
-									@endif
-								</div>
-							</div>
+						</div>
 						<!-- <div class="col-md-2 services_heading_right">
 							<div class="toolbox-banner mail_icon-link">
 								<p class="pull-right"><a class="panel-heading_link" href="/accounts/edit/{{ $AccountDetails[0]->id }}">Edit</a></p>
@@ -99,13 +101,16 @@
 					<div class="col-md-7 services_heading_right">
 
 						<h4>Contact Details</h4>
-						<p>{{ $UserAddress[0]->address_1 }} <br class="show-sm-down"/>{{ $UserAddress[0]->suburb_town }} VIC {{ $UserAddress[0]->postcode }}</p>
-						<p>
-							{{ $AccountDetails[0]->email }}
-						</p>
-						<p>
-							{{ $AccountDetails[0]->mobile }}
-						</p>
+						<div class="col-md-6 contact-address-details">
+							<p>{{ $UserAddress[0]->address_1 }} <br class="show-sm-down"/>{{ $UserAddress[0]->suburb_town }} VIC {{ $UserAddress[0]->postcode }}</p>
+						</div>
+						<div class="col-md-6 contact-address-details">
+							<p>
+								{{ $AccountDetails[0]->email }}
+								<br/>
+								{{ $AccountDetails[0]->mobile }}
+							</p>
+						</div>
 						<h4>Services</h4>
 						<p>
 							Victorian Housing Register
