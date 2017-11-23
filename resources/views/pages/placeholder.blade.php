50a220b3d6533d5a033aea528cc4097e6babe32f
TattyFromMelbourne <tlenz@lensoftware.com>
2017-11-23 23:51:00 +1100

    Fixed iPad front page layout bug

--- resources/views/pages/placeholder.blade.php
+++ resources/views/pages/placeholder.blade.php
@@ -10,16 +10,16 @@
 			<div class="col-md-8 col-lg-6  col-sm-12  col-xs-12  welcome_banner-wrapper">
 				<div class="placeholder-banner">
 					<h3>Uh oh! This page is not available. </h3>
-				
-					<h5>The page that you are viewing doesnt seem to be available at the moment.</h5>
-					<h5>This might be part of the future state of the concept or the page is under construction.</h5>
+
+					<h5>The page that you are trying to reach is not yet implemented.</h5>
+					<h5>It is intended as a future concept.</h5>
 				</div>
 				<div class="placeholder-banner">
-					<button type="button" class="btn btn-primary"><a class="panel-heading_link edit_button" href="/"><i class="fa fa-angle-double-left" aria-hidden="true"></i> Go back to first page</a></button>
+					<button type="button" class="btn btn-primary btn-vhs"><a class="panel-heading_link edit_button" href="/"><i class="fa fa-angle-double-left" aria-hidden="true"></i> Go back to first page</a></button>
 					</div>
 				</div>
 			</div>
-		</div>		
+		</div>
 	</div>
 
 	@endsection
