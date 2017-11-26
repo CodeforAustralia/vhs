0fe03982df13a429a85e3587e80678bfba07e93b
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-11-26 21:38:58 +1100

    View Profile Accessibility and Style Fix - Closes #27
    Signed-off-by: Rachelle Salvadora <rachellesalvadora@gmail.com>

--- resources/views/pages/account/view.blade.php
+++ resources/views/pages/account/view.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-@if ( Auth::user()->usertype == '1' )<!-- Modal -->
+@if ( Auth::user()->usertype == '1' ) {{-- Modal --}}
 <div id="modalWindow" class="modal fade" role="dialog">
 	<div class="modal-dialog">
-		<!-- Modal content-->
+		{{-- Modal content --}}
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -35,7 +35,7 @@
 </div>
 @endif
 
-<!-- Get session messages -->
+{{-- Get session messages --}}
 @if (Session::has('message.content'))
 <div id="sessionStatus" class="alert alert-{{ Session::get('message.level') }}">
 	{{Session::get('message.content')}}
@@ -43,61 +43,63 @@
 </div>
 @endif
 
-<div class="container">
-	<div class="row">
-		<div class="noSpace">
-			<a class="backButton" href="">&lt;&nbsp;&nbsp;Back</a>
-		</div>
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<div class="welcome-banner">
-						<div class="col-md-2">
+<div class="container-wrapper">
+	<div class="container">
+		<div class="row">
+			<div class="noSpace">
+				<a class="backButton" href="">&lt;&nbsp;&nbsp;Back</a>
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<div class="welcome-banner">
+							<div class="col-md-2">
 
-						</div>
-						<div class="col-md-7 services_heading_right">
-							@if ( Auth::user()->usertype == '1' )
-							<h3>{{ $AccountDetails[0]->firstName }} {{ $AccountDetails[0]->lastName }}</h3>
-							<p>
-								@if($AccountDetails[0]->last_login_at == NULL)
-								{{ 'This user has never logged in before' }}
-								@else
-								{{$lastLoginAt = Request::get($AccountDetails[0]->last_login_at) }}{{ 'Last login '. date("F jS, Y h:i:s A", strtotime($lastLoginAt)) }}
-								@endif
-							</p>
-							@else
-							<h3>{{ $AccountDetails[0]->firstName }} {{ $AccountDetails[0]->lastName }}</h3>
-							<p>
-								@if($AccountDetails[0]->last_login_at == NULL)
-								{{ 'This user has never logged in before' }}
-								@else
-								{{$lastLoginAt = Request::get($AccountDetails[0]->last_login_at) }}{{ 'Last login '. date("F jS, Y h:i:s A", strtotime($lastLoginAt)) }}
-								@endif
-							</p>
-							@endif
-						</div>
-						<div class="col-md-3 services_heading_right">
-							<div class="toolbox-banner mail_icon-link">
+							</div>
+							<div class="col-md-7 services_heading_right">
 								@if ( Auth::user()->usertype == '1' )
-								<p><a class="panel-heading_link" href="/notification/{{ $AccountDetails[0]->id }}">Send Notification</a></p>
-								<p><a class="panel-heading_link" href="#modalWindow" data-toggle="modal" data-target="#modalWindow">Send Sample Letters</a></p>
-								<p><a href="/accounts">All Users</a></p>
+								<h3>{{ $AccountDetails[0]->firstName }} {{ $AccountDetails[0]->lastName }}</h3>
+								<p>
+									@if($AccountDetails[0]->last_login_at == NULL)
+									{{ 'This user has never logged in before' }}
+									@else
+									{{$lastLoginAt = Request::get($AccountDetails[0]->last_login_at) }}{{ 'Last login '. date("F jS, Y h:i:s A", strtotime($lastLoginAt)) }}
+									@endif
+								</p>
+								@else
+								<h3>{{ $AccountDetails[0]->firstName }} {{ $AccountDetails[0]->lastName }}</h3>
+								<p>
+									@if($AccountDetails[0]->last_login_at == NULL)
+									{{ 'This user has never logged in before' }}
+									@else
+									{{$lastLoginAt = Request::get($AccountDetails[0]->last_login_at) }}{{ 'Last login '. date("F jS, Y h:i:s A", strtotime($lastLoginAt)) }}
+									@endif
+								</p>
 								@endif
 							</div>
+
+							<div class="col-md-3 services_heading_right">
+								<div class="toolbox-banner mail_icon-link">
+									@if ( Auth::user()->usertype == '1' )
+									<p><a class="panel-heading_link" href="/notification/{{ $AccountDetails[0]->id }}">Send Notification</a></p>
+									<p><a class="panel-heading_link" href="#modalWindow" data-toggle="modal" data-target="#modalWindow">Send Sample Letters</a></p>
+									<p><a href="/accounts">All Users</a></p>
+									@endif
+								</div>
+							</div>
 						</div>
-						<!-- <div class="col-md-2 services_heading_right">
-							<div class="toolbox-banner mail_icon-link">
-								<p class="pull-right"><a class="panel-heading_link" href="/accounts/edit/{{ $AccountDetails[0]->id }}">Edit</a></p>
-							</div>
-						</div> -->
-						<!-- <div class="col-md-1 services_heading_right">
-							<div class="toolbox-bannetest@test.com.aur mail_icon-link">
-							</div>
-						</div> -->
 					</div>
 				</div>
-				<div class="panel-body">
-
+			</div>
+		</div>
+	</div>
+</div>
+{{-- Account Information --}}
+<div class="container-wrapper services-wrapper">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="panel-body account_panel">
 					<div class="col-md-2">
 
 					</div>
@@ -149,19 +151,14 @@
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-	</div>
-<div class="edit_profile-wrapper">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12 col-md-8 col-lg-6 col-xs-12 edit-button-wrapper">
-				<a class="panel-heading_link edit_button" href="/accounts/edit/{{ $AccountDetails[0]->id }}"><button type="button" class="btn btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i> Edit Profile</button></a>
+				<div class="col-sm-12 col-md-8 col-xs-12 edit-button-wrapper">
+					<a class="panel-heading_link edit_button" href="/accounts/edit/{{ $AccountDetails[0]->id }}"><button type="button" class="btn btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i> Edit Profile</button></a>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
-</div>
-@include('pages.account.hoInfo')
+@include('letter/interpreting')
+@include('pages.dashboard.housing_office')
 
 @endsection
